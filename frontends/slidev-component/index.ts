fca44874da63d02d06b00971189d0201b15347d7
asm0dey <pavel.finkelshtein@gmail.com>
2026-05-12 17:53:26 +0200

    feat(slidev-component): export poll results store from package entry

--- frontends/slidev-component/index.ts
+++ frontends/slidev-component/index.ts
@@ -2,3 +2,10 @@ import "@slidev-polls/shared/tokens.css";
 
 export { default as PollResults } from "./components/PollResults.vue";
 export { configureDeckAuthBackend } from "./composables/configureDeckAuthBackend";
+export {
+  usePollResults,
+  usePollResultsMap,
+  setPollResults,
+  clearPollResults
+} from "./composables/usePollResults";
+export type { SnapshotEvent, TallyEntry } from "@slidev-polls/shared";
