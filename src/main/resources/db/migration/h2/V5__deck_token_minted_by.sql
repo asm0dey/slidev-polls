367fff40da24818a138e03366195d6e56a38e471
asm0dey <pavel.finkelshtein@gmail.com>
2026-05-23 14:13:36 +0200

    fix(db): backfill WHERE guard + VARCHAR over CHAR in session schema

--- src/main/resources/db/migration/h2/V5__deck_token_minted_by.sql
+++ src/main/resources/db/migration/h2/V5__deck_token_minted_by.sql
@@ -1,4 +1,4 @@
 ALTER TABLE deck_tokens ADD COLUMN minted_by varchar(64);
-UPDATE deck_tokens dt SET minted_by = (SELECT p.owner_username FROM polls p WHERE p.id = dt.poll_id);
+UPDATE deck_tokens dt SET minted_by = (SELECT p.owner_username FROM polls p WHERE p.id = dt.poll_id) WHERE dt.minted_by IS NULL;
 ALTER TABLE deck_tokens ALTER COLUMN minted_by SET NOT NULL;
 ALTER TABLE deck_tokens ADD CONSTRAINT deck_tokens_minted_by_fk FOREIGN KEY (minted_by) REFERENCES admin_user(username);
